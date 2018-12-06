91ba7aca4b97d11c26564c4ab85cfd58a6092e9e
shreeya-joshi <shreeyajoshi96@gmail.com>
2018-12-06 13:27:07 -0800

    if else most outside

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -85,7 +85,7 @@ $(document).ready(function () {
          console.log('baadd');
 	document.getElementById("indicator1");
 		document.getElementById("indicator2").style.display = "none";
-}else if {
+}else if(obj.temperature<800){
         console.log('gooodd');
 		document.getElementById("indicator2");
 		document.getElementById("indicator1").style.display = "none";
