d033d0c159c430c3c3ee591a67b300c98513da21
shreeya-joshi <shreeyajoshi96@gmail.com>
2018-12-13 22:44:11 -0800

    colour35

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -34,7 +34,7 @@ $(document).ready(function () {
     title: {
       display: true,
       text: 'Air Quality Real-time Data',
-      fontSize: 50
+      fontSize: 100
     },
     scales: {
       yAxes: [{
